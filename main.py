9112cc6645c9de4f022b5e584feddaa3de50efc6
Jeremiah Norris <jjno91@gmail.com>
2023-01-24 14:07:18 -0600

    lookup backend buckets

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-import re
+from os import rename
+from os.path import exists
+from re import sub
 
+from boto3 import client
 # noinspection PyPackageRequirements
 from decouple import config
 
@@ -8,7 +10,22 @@ backend_prefix = config('BACKEND_PREFIX', default='terraform-backend')
 state_prefix = config('STATE_PREFIX', default='')
 branch = config('GITHUB_REF_NAME').replace('/', '-')
 repo = config('GITHUB_REPOSITORY')
-name = re.sub('.*/', '', repo)
+name = sub('.*/', '', repo)
+client = client('s3')
+response = client.list_buckets()
+buckets = []
 
-if os.path.exists(branch + '.tfvars'):
-    os.rename(branch + '.tfvars', branch + 'auto.tfvars')
+if exists(branch + '.tfvars'):
+    rename(branch + '.tfvars', branch + 'auto.tfvars')
+
+for bucket in response['Buckets']:
+    if bucket["Name"].startswith(backend_prefix):
+        buckets.append(bucket["Name"])
+
+if len(buckets) == 1:
+    bucket = buckets.pop()
+elif len(buckets) < 1:
+    print('find bucket')
+elif len(buckets) > 1:
+    print('Multiple backends found: ', buckets)
+    exit()
